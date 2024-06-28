7405a2d004eeb5a9c577d303043776ad61095923
Gatadhitalia <gataanindhitaz@gmail.com>
2024-06-28 14:00:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -130,11 +130,65 @@ Route::get('/hasilquiz', function () {
 Route::get('/tracerstudy/quiz', 'App\Http\Controllers\QuizController@create')->name('tracerstudy.quiz');
 Route::post('/tracerstudy/quiz', 'App\Http\Controllers\QuizController@store')->name('tracerstudy.store');
 
-//route per masing masing quiz
-Route::resource('/academic', \App\Http\Controllers\academicController::class);
-Route::resource('/job', \App\Http\Controllers\jobController::class);
-Route::resource('/internship', \App\Http\Controllers\InternshipController::class);
-Route::resource('/organization', \App\Http\Controllers\OrganizationController::class);
-Route::resource('/award', \App\Http\Controllers\AwardController::class);
-Route::resource('/course', \App\Http\Controllers\CourseController::class);
-Route::resource('/skill', \App\Http\Controllers\SkillController::class);
+// Academic Controller
+Route::get('/academic', [\App\Http\Controllers\AcademicController::class, 'index'])->name('academic.index');
+Route::get('/academic/create', [\App\Http\Controllers\AcademicController::class, 'create'])->name('academic.create');
+Route::post('/academic', [\App\Http\Controllers\AcademicController::class, 'store'])->name('academic.store');
+Route::get('/academic/{academic}', [\App\Http\Controllers\AcademicController::class, 'show'])->name('academic.show');
+Route::get('/academic/{academic}/edit', [\App\Http\Controllers\AcademicController::class, 'edit'])->name('academic.edit');
+Route::put('/academic/{academic}', [\App\Http\Controllers\AcademicController::class, 'update'])->name('academic.update');
+Route::delete('/academic/{academic}', [\App\Http\Controllers\AcademicController::class, 'destroy'])->name('academic.destroy');
+
+// Job Controller
+Route::get('/job', [\App\Http\Controllers\JobController::class, 'index'])->name('job.index');
+Route::get('/job/create', [\App\Http\Controllers\JobController::class, 'create'])->name('job.create');
+Route::post('/job', [\App\Http\Controllers\JobController::class, 'store'])->name('job.store');
+Route::get('/job/{job}', [\App\Http\Controllers\JobController::class, 'show'])->name('job.show');
+Route::get('/job/{job}/edit', [\App\Http\Controllers\JobController::class, 'edit'])->name('job.edit');
+Route::put('/job/{job}', [\App\Http\Controllers\JobController::class, 'update'])->name('job.update');
+Route::delete('/job/{job}', [\App\Http\Controllers\JobController::class, 'destroy'])->name('job.destroy');
+
+// Internsip controller
+Route::get('/internship', [\App\Http\Controllers\InternshipController::class, 'index'])->name('internship.index');
+Route::get('/internship/create', [\App\Http\Controllers\InternshipController::class, 'create'])->name('internship.create');
+Route::post('/internship', [\App\Http\Controllers\InternshipController::class, 'store'])->name('internship.store');
+Route::get('/internship/{internship}', [\App\Http\Controllers\InternshipController::class, 'show'])->name('internship.show');
+Route::get('/internship/{internship}/edit', [\App\Http\Controllers\InternshipController::class, 'edit'])->name('internship.edit');
+Route::put('/internship/{internship}', [\App\Http\Controllers\InternshipController::class, 'update'])->name('internship.update');
+Route::delete('/internship/{internship}', [\App\Http\Controllers\InternshipController::class, 'destroy'])->name('internship.destroy');
+
+// Organization controller
+Route::get('/organization', [\App\Http\Controllers\OrganizationController::class, 'index'])->name('organization.index');
+Route::get('/organization/create', [\App\Http\Controllers\OrganizationController::class, 'create'])->name('organization.create');
+Route::post('/organization', [\App\Http\Controllers\OrganizationController::class, 'store'])->name('organization.store');
+Route::get('/organization/{organization}', [\App\Http\Controllers\OrganizationController::class, 'show'])->name('organization.show');
+Route::get('/organization/{organization}/edit', [\App\Http\Controllers\OrganizationController::class, 'edit'])->name('organization.edit');
+Route::put('/organization/{organization}', [\App\Http\Controllers\OrganizationController::class, 'update'])->name('organization.update');
+Route::delete('/organization/{organization}', [\App\Http\Controllers\OrganizationController::class, 'destroy'])->name('organization.destroy');
+
+// Award controller
+Route::get('/award', [\App\Http\Controllers\AwardController::class, 'index'])->name('award.index');
+Route::get('/award/create', [\App\Http\Controllers\AwardController::class, 'create'])->name('award.create');
+Route::post('/award', [\App\Http\Controllers\AwardController::class, 'store'])->name('award.store');
+Route::get('/award/{award}', [\App\Http\Controllers\AwardController::class, 'show'])->name('award.show');
+Route::get('/award/{award}/edit', [\App\Http\Controllers\AwardController::class, 'edit'])->name('award.edit');
+Route::put('/award/{award}', [\App\Http\Controllers\AwardController::class, 'update'])->name('award.update');
+Route::delete('/award/{award}', [\App\Http\Controllers\AwardController::class, 'destroy'])->name('award.destroy');
+
+// Organization controller
+Route::get('/course', [\App\Http\Controllers\CourseController::class, 'index'])->name('course.index');
+Route::get('/course/create', [\App\Http\Controllers\CourseController::class, 'create'])->name('course.create');
+Route::post('/course', [\App\Http\Controllers\CourseController::class, 'store'])->name('course.store');
+Route::get('/course/{course}', [\App\Http\Controllers\CourseController::class, 'show'])->name('course.show');
+Route::get('/course/{course}/edit', [\App\Http\Controllers\CourseController::class, 'edit'])->name('course.edit');
+Route::put('/course/{course}', [\App\Http\Controllers\CourseController::class, 'update'])->name('course.update');
+Route::delete('/course/{course}', [\App\Http\Controllers\CourseController::class, 'destroy'])->name('course.destroy');
+
+// Award controller
+Route::get('/skill', [\App\Http\Controllers\SkillController::class, 'index'])->name('skill.index');
+Route::get('/skill/create', [\App\Http\Controllers\SkillController::class, 'create'])->name('skill.create');
+Route::post('/skill', [\App\Http\Controllers\SkillController::class, 'store'])->name('skill.store');
+Route::get('/skill/{skill}', [\App\Http\Controllers\SkillController::class, 'show'])->name('skill.show');
+Route::get('/skill/{skill}/edit', [\App\Http\Controllers\SkillController::class, 'edit'])->name('skill.edit');
+Route::put('/skill/{skill}', [\App\Http\Controllers\SkillController::class, 'update'])->name('skill.update');
+Route::delete('/skill/{skill}', [\App\Http\Controllers\SkillController::class, 'destroy'])->name('skill.destroy');
