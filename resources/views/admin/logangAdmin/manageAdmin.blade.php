bb247869eabd5b08f0376e8b36833fd56c985a43
mahsarahima <91888752+MahsaRahima@users.noreply.github.com>
2024-07-08 10:35:41 +0700

    admin lowongan

--- resources/views/admin/logangAdmin/manageAdmin.blade.php
+++ resources/views/admin/logangAdmin/manageAdmin.blade.php
@@ -145,7 +145,7 @@
         <img src="{{ asset('images/logo-udinus.png') }}" alt="Logo udinus" width="55">
         <img src="{{ asset('images/logo-unggul.png') }}" alt="Logo udinus" width="40">
     </a>
-    <form action="/manageAdmin" class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+    <form action="/manageLogangAdmin" class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <div class="input-group">
             <input class="form-control" type="text" placeholder="Search here..." aria-label="Search for..." name="search" aria-describedby="btnNavbarSearch"/>
             <button class="btn" id="btnNavbarSearch" type="submit"><i class="fas fa-search"></i></button>
