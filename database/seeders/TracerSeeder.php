fb3d4962a912412176f193739b98d9e4a48690cc
mahsarahima <91888752+MahsaRahima@users.noreply.github.com>
2024-08-15 19:06:46 +0700

    Update TracerSeeder.php

--- database/seeders/TracerSeeder.php
+++ database/seeders/TracerSeeder.php
@@ -6,7 +6,7 @@ use App\Models\kuesioner;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class LogangSeeder extends Seeder
+class TracerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,54 +14,64 @@ class LogangSeeder extends Seeder
     public function run(): void
     {
         kuesioner::create([
-            'NamaPerusahaan' => 'Telkom Indonesia',
-            'Posisi' => 'Developer',
-            'Alamat' => 'Jl. Gemah Raya I, Gemah, Kec. Pedurungan, Kota Semarang, Jawa Tengah 50246',
-            'Pengalaman' => 'Tanpa Pengalaman',
-            'Gaji' => '3 Juta',
-            'TipeMagang' => 'Full Time',
-            'Deskripsi' => 'Menguasai pembuatan aplikasi mobile
-                            Mampu berkomunikasi dengan baik dan aktif dalama bahasa Indonesia maupun bahasa Inggris',
-            'Website' => 'https://recruitment.telkom.co.id/',
-            'Email' => 'Telkomcare@gmail.com',
-            'Tags' => 'Telkom Indonesia, Developer, Full Time',
-            'Logo' => 'Telkom.jpg',
-            'Verify' => 'verified'
+            'id_alumni' => '2',
+            'nama_alumni' => 'Gata Anindhita Zalianingrum',
+            'jns_kelamin' => 'Perempuan',
+            'nim' => 'A11.2021.13439',
+            'tahun_masuk' => '2020',
+            'tahun_lulus' => '2024',
+            'no_hp' => '0812',
+            'email' => 'gata@email',
+            'status' => 'Melanjutkan Pendidikan',
+            'jns_job' => '',
+            'nama_job' => '',
+            'jabatan_job' => '',
+            'lingkup_job' => '',
+            'biaya_studi' => 'Beasiswa',
+            'nama_studi' => 'Udinus',
+            'prodi' => 'TI',
+            'tgl_studi' => '2024-10-01'
         ]);
 
         kuesioner::create([
-            'NamaPerusahaan' => 'Djarum',
-            'Posisi' => 'Computer User Support',
-            'Alamat' => 'Jl. Jend. Ahmad Yani No.26-28, Krajan, Panjunan, Kec. Kota Kudus, Kabupaten Kudus, Jawa Tengah 59317',
-            'Pengalaman' => 'Tanpa Pengalaman',
-            'Gaji' => '3-5 Juta',
-            'TipeMagang' => 'Kontrak',
-            'Deskripsi' => 'Minimal IPK 3.5
-                            Minimal Semester 5
-                            Bersedia di tempatkan di Kudus',
-            'Website' => 'https://career.djarum.com/home',
-            'Email' => 'DjarumCare@gmail.com',
-            'Tags' => 'Djarum, Kontrak, Tanpa Pengalaman',
-            'Logo' => 'Djarum.jpg',
-            'Verify' => 'verified'
+            'id_alumni' => '3',
+            'nama_alumni' => 'Mahsa Rahima Yunus',
+            'jns_kelamin' => 'Perempuan',
+            'nim' => 'A11.2021.13457',
+            'tahun_masuk' => '2020',
+            'tahun_lulus' => '2024',
+            'no_hp' => '0811',
+            'email' => 'mahsa@email',
+            'status' => 'Menikah/Mengurus Keluarga',
+            'jns_job' => '',
+            'nama_job' => '',
+            'jabatan_job' => '',
+            'lingkup_job' => '',
+            'biaya_studi' => '',
+            'nama_studi' => '',
+            'prodi' => '',
+            'tgl_studi' => null
         ]);
 
         kuesioner::create([
-            'NamaPerusahaan' => 'OVO',
-            'Posisi' => 'IT Mobile',
-            'Alamat' => 'Pakuwon Tower, Jl. Raya Casablanca No.Raya Lt. 46, RT.3/RW.14, Menteng Dalam, Kec. Tebet, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12870',
-            'Pengalaman' => 'Tanpa Pengalaman',
-            'Gaji' => '2 Juta',
-            'TipeMagang' => 'Kontrak',
-            'Deskripsi' => 'Menguasai Bahasa Pemrograman Dart
-                            Dapat berkomunikasi menggunakan bahasa Inggris
-                            Dapat bekerja dibawah tekanan
-                            Mampu beradaptasi dengan baik',
-            'Website' => 'https://www.ovo.id/',
-            'Email' => 'Ovo@gmail.com',
-            'Tags' => 'Ovo, Mobile, Tanpa Pengalaman',
-            'Logo' => 'Ovo.jpg',
-            'Verify' => 'pending'
+            'id_alumni' => '4',
+            'nama_alumni' => 'Fauzan Febryan Tyowarsa',
+            'jns_kelamin' => 'Laki-Laki',
+            'nim' => 'A11.2021.13422',
+            'tahun_masuk' => '2020',
+            'tahun_lulus' => '2024',
+            'no_hp' => '0822',
+            'email' => 'fauxan@email',
+            'status' => 'Belum Memungkinkan Bekerja',
+            'jns_job' => '',
+            'nama_job' => '',
+            'jabatan_job' => '',
+            'lingkup_job' => '',
+            'biaya_studi' => '',
+            'nama_studi' => '',
+            'prodi' => '',
+            'tgl_studi' => null
         ]);
+
     }
 }
