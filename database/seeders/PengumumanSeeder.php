7fad4baa12358730066acd168e0473d4fdcc9184
mahsarahima <91888752+MahsaRahima@users.noreply.github.com>
2024-08-24 22:21:31 +0700

    API Pengumuman dan PengumumanLogang

--- database/seeders/PengumumanSeeder.php
+++ database/seeders/PengumumanSeeder.php
@@ -16,21 +16,25 @@ class PengumumanSeeder extends Seeder
         Pengumuman::create([
             'judul' => 'Alumni',
             'user' => 'Koordinator Alumni',
+            'published_at' => now(),
             'isi' => 'Bagi alumni yang belum memiliki akun, dapat melakukan registrasi terlebih dahulu untuk mendapatkan akses ke sistem dan informasi terbaru.'
         ]);
         Pengumuman::create([
             'judul' => 'Tracerstudy',
             'user' => 'Koordinator Alumni',
+            'published_at' => now(),
             'isi' => 'Kepada seluruh alumni, dimohon untuk segera mengisi form tracer study guna membantu kami dalam meningkatkan kualitas pendidikan program studi. Partisipasi Anda sangat berharga, terima kasih!'
         ]);
         Pengumuman::create([
             'judul' => 'Lowongan Kerja',
             'user' => 'Koordinator Alumni',
+            'published_at' => now(),
             'isi' => 'Tersedia 15 lowonvan dengan tipe kerja freelance yang siap Anda lamar untuk memulai atau melanjutkan karir profesional Anda. Yuk cek lowonganmu pada sistem Alumni!'
         ]);
         Pengumuman::create([
             'judul' => 'Lowongan Magang',
             'user' => 'Koordinator Alumni',
+            'published_at' => now(),
             'isi' => 'Tersedia 10 peluang magang dengan tipe magang part time untuk menambah pengalaman dan meningkatkan keterampilan Anda di dunia kerja. Yuk cek lowonganmu pada sistem Alumni!'
         ]);
     }
